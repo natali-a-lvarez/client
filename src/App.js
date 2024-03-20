5121da4fb955d44e21401fa33b69d038b51cbd78
natali-a-lvarez <03nataliaalvarez07@gmail.com>
2024-03-20 15:58:07 -0400

    creating a basic flask and react project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("/users")
+      .then((res) => res.json())
+      .then((data) => setData(data.users));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>This is my app</h1>
+      {data.map((user, i) => (
+        <p key={i}>{user}</p>
+      ))}
+    </>
   );
 }
 
