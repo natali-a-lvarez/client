ac16021dbe33bd63aef0990f6d797959f87d4640
natali-a-lvarez <03nataliaalvarez07@gmail.com>
2024-03-21 14:57:26 -0400

    creating and implementing CRUD with FLASK

--- src/App.js
+++ src/App.js
@@ -2,19 +2,37 @@ import React, { useState, useEffect } from "react";
 
 function App() {
   const [data, setData] = useState([]);
+  const [bookDetails, setBookDetails] = useState(false);
+  const [book, setBook] = useState({});
 
   useEffect(() => {
-    fetch("/users")
+    fetch("http://127.0.0.1:5000/books")
       .then((res) => res.json())
-      .then((data) => setData(data.users));
+      .then((data) => setData(data));
   }, []);
+
+  async function clickHandler(id) {
+    const res = await fetch(`http://127.0.0.1:5000/books/${id}`);
+    const data = await res.json();
+    setBook(data);
+    setBookDetails(true);
+  }
 
   return (
     <>
-      <h1>This is my app</h1>
-      {data.map((user, i) => (
-        <p key={i}>{user}</p>
-      ))}
+      <h1>These are my books</h1>
+      {!bookDetails &&
+        data.map((book) => (
+          <div onClick={() => clickHandler(book.id)}>
+            <h3>{book.title}</h3>
+          </div>
+        ))}
+      {bookDetails && (
+        <div>
+          <h3>{book.title}</h3>
+          <p>{book.author}</p>
+        </div>
+      )}
     </>
   );
 }
