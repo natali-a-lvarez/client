a16a858a6ab3e6efbc8b99610794b9e80f3ec247
natali-a-lvarez <03nataliaalvarez07@gmail.com>
2024-03-22 15:21:29 -0400

    changing the set data function

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   useEffect(() => {
     fetch("http://127.0.0.1:5000/books")
       .then((res) => res.json())
-      .then((data) => setData(data));
+      .then((data) => setData(data.books));
   }, [data]);
 
   function goHomeHandler() {
